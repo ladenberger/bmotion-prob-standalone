86659cf2c206b0389b4d96db9ae57ec0c5c12d9e
Lukas Ladenberger <lukas.ladenberger@googlemail.com>
2015-05-19 10:43:43 +0200

    do not copy and paste :)

--- env.sh
+++ env.sh
@@ -1,4 +1,5 @@
-export NVM_BIN=$(HOME)/.nvm/versions/node/v0.12.0/bin
-export NVM_DIR=$(HOME)/.nvm
-export NVM_PATH=$(HOME)/.nvm/versions/node/v0.12.0/lib/node
-export PATH:=$(NVM_BIN):$(PATH)
+#!/bin/sh
+export NVM_BIN=$HOME/.nvm/versions/node/v0.12.0/bin
+export NVM_DIR=$HOME/.nvm
+export NVM_PATH=$HOME/.nvm/versions/node/v0.12.0/lib/node
+export PATH=$NVM_BIN:$PATH
